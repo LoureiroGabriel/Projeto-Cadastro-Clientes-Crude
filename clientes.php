d555784f0c5156870e2c8a29c3622d5140a53fd3
Gabriel Loureiro <loureirogabrieloficial@gmail.com>
2023-11-19 13:48:38 -0300

    Tratamento de Dados

--- clientes.php
+++ clientes.php
@@ -3,7 +3,7 @@ include("./conexao.php");
 
 $sql_clientes = "SELECT * FROM clientes";
 $query_clientes = $mysqli->query($sql_clientes) or die($mysqli->error);
-$num_clientes = $query_clientes->num_rows; // Corrigido aqui
+$num_clientes = $query_clientes->num_rows;
 
 ?>
 
@@ -33,21 +33,35 @@ $num_clientes = $query_clientes->num_rows; // Corrigido aqui
                 <tr>
                     <td colspan="6">Nenhum cliente foi cadastrado</td>
                 </tr>
-            <?php } else {
+                <?php } else {
                 while ($cliente = $query_clientes->fetch_assoc()) {
-            ?>
+
+                    $telefone = "Não informado";
+                    if (!empty($cliente['telefone'])) {
+                        $telefone = formatar_telefone($cliente['telefone']);
+                    }
+
+                    $nascimento = "Não informada";
+                    if (!empty($cliente['nascimento'])) {
+                        $nascimento = formatar_data($cliente['nascimento']);
+                    }
+
+                    $data_cadastro = date("d/m/Y  H:i", strtotime($cliente['data']))
+
+
+                ?>
                     <tr>
                         <td><?php echo $cliente["id"] ?></td>
                         <td><?php echo $cliente["nome"] ?></td>
                         <td><?php echo $cliente["email"] ?></td>
-                        <td><?php echo $cliente["telefone"] ?></td>
-                        <td><?php echo $cliente["nascimento"] ?></td>
-                        <td><?php echo $cliente["data"] ?></td>
+                        <td><?php echo $telefone ?></td>
+                        <td><?php echo $nascimento ?></td>
+                        <td><?php echo $data_cadastro ?></td>
                         <td>
                             <a href="editar_cliente.php?id=<?php echo $cliente["id"]; ?>">Editar</a>
-                            <a href="deletar_cliente.php"?id=<?php echo $cliente["id"]; ?>>Deletar</a>
+                            <a href="deletar_cliente.php" ?id=<?php echo $cliente["id"]; ?>>Deletar</a>
                         </td>
-                         
+
                     </tr>
             <?php
                 }
