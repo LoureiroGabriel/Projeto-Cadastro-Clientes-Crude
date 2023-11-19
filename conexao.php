6e93f7b89d138a72b72f7034f5a990f4b29c3853
Gabriel Loureiro <loureirogabrieloficial@gmail.com>
2023-11-19 13:47:07 -0300

    implementação funcões de formatação de data e telefone

--- conexao.php
+++ conexao.php
@@ -5,8 +5,24 @@ $usuario = "root";
 $senha = "";
 
 $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedadoos);
-if($mysqli->connect_errno){
+if ($mysqli->connect_errno) {
     echo "falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_errno;
-}else{
-    echo "Conectado com Sucesso";
+}
+
+
+function formatar_data($data)
+{
+    return implode('/', array_reverse(explode('-', $data)));
+}
+
+
+function formatar_telefone($telefone)
+{
+    if (!empty($telefone)) {
+        $ddd = substr($telefone, 0, 2);
+        $parte1 = substr($telefone, 2, 5);
+        $parte2 = substr($telefone, 7);
+        $telefone_formatado = "($ddd) $parte1-$parte2";
+        return $telefone_formatado;
+    }
 }
